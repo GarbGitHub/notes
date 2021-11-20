f77f64d4c6070cbee63379757caf2311f6e024b5
Sergei_Klyuikov <garbich@gmail.com>
2021-11-20 23:42:35 +0300

    add rout favorites

--- post_inn/api/urls.py
+++ post_inn/api/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from api.views import NoteViewSet, UserViewsSet, NoteBasketViewSet
+from api.views import NoteViewSet, UserViewsSet, NoteBasketViewSet, NoteFavoritesViewSet
 
 router = DefaultRouter()
 router.register('notes', NoteViewSet, basename='notes')
+router.register('favorites', NoteFavoritesViewSet, basename='favorites')
 router.register('basket', NoteBasketViewSet, basename='basket')
 router.register('users', UserViewsSet, basename='users')
 urlpatterns = router.urls
