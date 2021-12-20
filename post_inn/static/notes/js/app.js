b24ae720fb5e346bbb470ac94c735c535689be76
root <root@194-58-108-107.cloudvps.regruhosting.ru>
2021-12-20 16:39:47 +0300

    fix bug app.js

--- post_inn/static/notes/js/app.js
+++ post_inn/static/notes/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('sw.js', { scope: '/' })
+        .register('/sw.js', { scope: '/' })
         .then(function(reg) {
             console.log('Achieng Service worker Registration worked!');
         })
