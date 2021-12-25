4e6f623db6c3d1d374b5338bf9632bacc20397f2
Sergei_Klyuikov <garbich@gmail.com>
2021-12-25 13:48:56 +0300

    add operating conditions

--- post_inn/fordev/views.py
+++ post_inn/fordev/views.py
@@ -1,9 +1,21 @@
 from django.views.generic import ListView, DetailView
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from fordev.models import Page
+
+
+class ForPageListView(ListView):
+    paginate_by = 30
+    model = Page
+    template_name = 'fordev/list-pages.html'
+    context_object_name = 'posts'
+    ordering = '-created'
+
+    def get_queryset(self):
+        return Page.objects.filter(is_active=True, category=2).order_by('-is_boxed', '-created')
+
+    def get_context_data(self, **kwargs):
+        context = super(ForPageListView, self).get_context_data(**kwargs)
+        context['title_page'] = 'Пользовательское соглашение'
+        return context
 
 
 class ForDevListView(ListView):
@@ -18,12 +30,8 @@ class ForDevListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ForDevListView, self).get_context_data(**kwargs)
-        context['title_page'] = 'API Заметочник для разработчиков'
+        context['title_page'] = 'Разработчику'
         return context
-
-    # @method_decorator(user_passes_test(lambda u: u.is_authenticated, login_url='auth:login'))
-    # def dispatch(self, *args, **kwargs):
-    #     return super().dispatch(*args, **kwargs)
 
 
 class ForDevDetailView(DetailView):
@@ -34,12 +42,6 @@ class ForDevDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['pk'] = self.object.id
-        # context['title_page'] = f'{context.get(self, self.object.title)}'
-        context['title_page'] = 'API'
-        context['cat_pk'] = f'Категория: "{context.get(self, self.object.category.pk)}"'
+        context['cat_pk'] = f'{context.get(self, self.object.category.name)}'
         return context
 
-    # @method_decorator(user_passes_test(lambda u: u.is_authenticated, login_url='auth:login'))
-    # def dispatch(self, *args, **kwargs):
-    #     obj = self.get_object()
-    #     return super().dispatch(*args, **kwargs)
