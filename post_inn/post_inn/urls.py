a4f6145b0443696555cc6f0f1ef399f6820f7144
Sergei_Klyuikov <garbich@gmail.com>
2022-01-27 12:18:47 +0300

    edit url routing in project

--- post_inn/post_inn/urls.py
+++ post_inn/post_inn/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('sw.js', (TemplateView.as_view(template_name='notes/sw.js', content_type='application/javascript', )), name='sw.js'),
     path('pwabuilder-sw.js', (TemplateView.as_view(template_name='notes/pwabuilder-sw.js', content_type='application/javascript', )), name='pwabuilder-sw.js'),
     path('fordev/', include(fordev_urls, namespace='fordev'), name='fordev'),
-    path('auth/', include(auth_urls, namespace='auth'), name='auth'),
+    path('app/auth/', include(auth_urls, namespace='auth'), name='auth'),
     path('admin/', admin.site.urls, name='admin'),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', 'rest_framework')),  # авторизация в api
