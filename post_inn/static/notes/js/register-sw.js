c10ad3314de123692006dd45cbed4ac0a8f9bb8a
Sergei_Klyuikov <garbich@gmail.com>
2022-06-20 13:23:59 +0300

    del static files

--- post_inn/static/notes/js/register-sw.js
+++
@@ -1,9 +0,0 @@
-// Register serviceWorker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/pwabuilder-sw.js', {scope: '/'})
-    .then((reg) => {
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-    }).catch((error) => {
-    console.log('Registration failed with ' + error);
-  });
-}
