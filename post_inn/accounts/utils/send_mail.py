baf9becba268aef27b3e3d56b191024c50407a93
Sergei_Klyuikov <garbich@gmail.com>
2022-11-24 16:52:53 +0300

    edit verify email

--- post_inn/accounts/utils/send_mail.py
+++ post_inn/accounts/utils/send_mail.py
@@ -1,5 +1,6 @@
 import smtplib
 import logging
+import base64
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.urls import reverse
@@ -9,15 +10,39 @@ from post_inn import settings
 from post_inn.utils import logger
 
 
+def create_encoded_byte_str(txt: str) -> str:
+    """Create encoded byte string"""
+    try:
+        txt_byte = txt.encode('utf-8')
+        txt_b64 = base64.urlsafe_b64encode(txt_byte).decode("utf-8")
+        return txt_b64
+
+    except Exception as err:
+        err_message = f'Error: при кодировании данных произошла ошибка. Мы уже работаем над ней'
+        logging.critical(f'При кодировании данных произошло исключение: {err}')
+        return err_message
+
+
+def create_decoded_byte_str(txt: str) -> str:
+    """Create decoded byte string"""
+    try:
+        txt_byte = txt.encode('utf-8')
+        txt_str = base64.urlsafe_b64decode(txt_byte).decode("utf-8")
+        return txt_str
+
+    except Exception as err:
+        logging.critical(f'При декодировании данных произошло исключение: {err}')
+
+
 def send_verify_mail(user):
     """
     Confirm registration by email
     :param user:
     :return: количество успешно доставленных сообщений (которое может быть 0 или 1)
     """
-
     try:
-        verify_link = reverse('authapp:verify', args=[user.email, user.activation_key])
+        email_b64 = create_encoded_byte_str(user.email)
+        verify_link = reverse('authapp:verify', args=[email_b64, user.activation_key])
         title = f'Подтверждение учетной записи {user.email}'
         message = f'Для подтверждения учетной записи {user.email}, на сайте {settings.DOMAIN_NAME} - пройдите по ссылке: ' \
                   f'{settings.DOMAIN_NAME}{verify_link}'
@@ -51,11 +76,12 @@ def send_reset_password(user):
               f' {settings.DOMAIN_NAME}{verify_link}. Если Вы не запрашивали смену пароля - проигнорируйте это письмо.'
 
     try:
-        send_message = send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
-        return send_message
+        result_send_message = send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+        return result_send_message
 
     except smtplib.SMTPException as err:
         logging.error(f'При отправке письма произошло исключение: {err}')
+        return 0
 
     except Exception as err:
         logging.error(f'При отправке письма произошло исключение: {err.args[0]}', exc_info=True)
